ec7d2730cfed793565c8158ecd0520187344476c
Alex Gheorghe <alex@digitalsynware.com>
2022-11-11 12:00:26 +0100

    new: add files to project

---
+++ src/main/java/com/github/alexeses/Main.java
@@ -0,0 +1,5 @@
+package com.github.alexeses;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/github/alexeses/pojo/Persona.java
@@ -0,0 +1,2 @@
+package com.github.alexeses.pojo;public class Persona {
+}
